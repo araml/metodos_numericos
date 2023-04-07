d6e7ec25ecfb9417f77a7b4eb246c70c0c4273c7
araml <leandrojavr@gmail.com>
2023-04-06 21:46:54 -0300

    Init repo

---
+++ TP1/eliminacion.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+def eliminacion_gaussiana_sin_pivoteo(m: np.array) -> np.array:
+    return np.eye(3)
+
+def eliminacion_gaussiana_con_pivoteo(m: np.array) -> np.array: 
+    return np.zeros((3, 3))
+
+def eliminacion_gaussiana_tridiagonal(m: np.array) -> np.array: 
+    return np.zeros((3, 3))
+
---
+++ TP1/experimentos.py
@@ -0,0 +1,4 @@
+from eliminacion import *
+
+print(eliminacion_gaussiana_sin_pivoteo(np.array([1, 2, 3])))
+print(eliminacion_gaussiana_con_pivoteo(np.array([])))
