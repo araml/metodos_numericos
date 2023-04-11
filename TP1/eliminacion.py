3e2cf1f51dd0aaf6bd9b4174ea29957c0af57dfd
cat-j <catalinajuarros@gmail.com>
2023-04-10 21:22:01 -0300

    Add numerical error checks and vector implementation for tridiagonal matrices

--- TP1/eliminacion.py
+++ TP1/eliminacion.py
@@ -1,15 +1,8 @@
 import numpy as np
-import sys
 
-def eliminacion_gaussiana_sin_pivoteo(m: np.array) -> np.array:
-    return np.eye(3)
-
-def eliminacion_gaussiana_con_pivoteo(m: np.array) -> np.array: 
-    return np.zeros((3, 3))
-
-def eliminacion_gaussiana_tridiagonal(m: np.array) -> np.array: 
-    return np.zeros((3, 3))
-
+NUMPY_MAX = np.finfo(np.float64).max
+NUMPY_MIN = np.finfo(np.float64).min
+NUMPY_EPSILON = np.finfo(np.float64).eps
 
 def gaussianElimination_noPivoting(M: np.array, b: np.array, epsilon=0):
     n = M.shape[0]
@@ -24,7 +17,10 @@ def gaussianElimination_noPivoting(M: np.array, b: np.array, epsilon=0):
             b[i] = b[i] - coefficient * b[k] 
 
 
-def gaussianElimination_rowPivoting(M: np.array, b: np.array, max=sys.float_info.max, min=sys.float_info.min):
+def gaussianElimination_rowPivoting(
+        M: np.array, b: np.array,
+        max=NUMPY_MAX, min=NUMPY_MIN, epsilon=NUMPY_EPSILON, magnitudeEpsilon=NUMPY_EPSILON):
+
     n = M.shape[0]
 
     for k in range(n):
@@ -34,14 +30,30 @@ def gaussianElimination_rowPivoting(M: np.array, b: np.array, max=sys.float_info
         b[[pivotRowIndex, k]] = b[[k, pivotRowIndex]]
         
         for i in range(k+1, n):
+            if abs(M[k][k]) <= epsilon:
+                print("Numerical error risk: dividing by small absolute value!")
             coefficient = M[i][k] / M[k][k]
+
+            if outOfBounds(coefficient, max, min) or outOfBounds(b[k], max, min) or np.any(M[k] >= max) or np.any(M[k] <= min):
+                print("Numerical error risk: multiplying by big absolute value!")
             rowToSubtract = coefficient * M[k]
             solutionToSubtract = coefficient * b[k]
-            # TODO: compare coefficient and M, b components with max and min
-            if np.any(np.isclose(M[i], rowToSubtract)) or np.isclose(b[i], solutionToSubtract):
+
+            if np.any(np.isclose(M[i], rowToSubtract, epsilon, epsilon)) or np.isclose(b[i], solutionToSubtract, epsilon, epsilon):
                 print("Catastrophic cancellation risk!")
+            if differentMagnitudes(M[i], rowToSubtract, magnitudeEpsilon):
+                print("Absorption risk!")
             M[i] = M[i] - rowToSubtract
             b[i] = b[i] - solutionToSubtract
+
+
+def outOfBounds(n, max, min):
+    return n >= max or n <= min
+
+# TODO: change this to a better criterion
+def differentMagnitudes(a1: np.array, a2: np.array, epsilon):
+    t1, t2 = a1 <= epsilon, a2 <= epsilon
+    return np.any(t1 != t2)
 
 
 def gaussianElimination_tridiagonal(M: np.array, b: np.array):
@@ -53,7 +65,17 @@ def gaussianElimination_tridiagonal(M: np.array, b: np.array):
         b[k+1] = b[k+1] - coefficient * b[k]
 
 
-def solve(M, b, triangulationFunction):
+def gaussianElimination_tridiagonal_vectors(a: np.array, b: np.array, c: np.array, d: np.array):
+    n = a.size
+    assert(b.size == n and c.size == n and d.size == n)
+
+    for k in range(0, n-1):
+        coefficient = a[k+1] / b[k]
+        b[k+1] -= coefficient * c[k] # subtract row above
+        d[k+1] -= coefficient * d[k]
+
+
+def solve(M: np.array, b: np.array, triangulationFunction) -> np.array:
     n = M.shape[0]
     assert(M.shape == (n, n))
     assert(b.size == n)
