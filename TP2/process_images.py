4c01c6d70b1ebd1a120eb4e768c247968787cf10
Catalina Juarros <catalinajuarros@gmail.com>
2023-05-15 01:03:10 -0300

    Calculate all eigenvectors from flattened image covariance matrix

---
+++ TP2/process_images.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+paths = []
+imgs = []
+
+for path in sorted(list(Path('build/caras').rglob('*/*.pgm'))):
+    paths.append(path)
+    imgs.append(plt.imread(path))
+
+square_images = np.stack(imgs[:4])
+flattened_images = square_images.reshape(square_images.shape[0], square_images[0].size)
+flattened_images = flattened_images - np.mean(flattened_images, axis=0) # subtract average from each
+covariance = flattened_images.T @ flattened_images
+n = covariance.shape[0]
+covariance = covariance / (n-1)
+
+la = np.linalg
+e, v = la.eigh(covariance) # TODO: use our own implementation
