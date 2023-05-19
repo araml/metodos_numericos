6f70742a0a0f84de91d6013410a7d75949e86042
Catalina Juarros <catalinajuarros@gmail.com>
2023-05-19 20:25:31 -0300

    Compress images with 2DPCA

--- TP2/py_stuff/process_images.py
+++ TP2/py_stuff/process_images.py
@@ -81,13 +81,13 @@ def reduce_dimensions(flattened_images: np.array, eigenbase: np.array, k: int) -
     return flattened_images @ (eigenbase[:, :k])
 
 
-def reconstruct_images(reduced_images: np.array, eigenbase: np.array, k: int) -> np.array:
+def reconstruct_images_1DPCA(reduced_images: np.array, eigenbase: np.array, k: int) -> np.array:
     return reduced_images @ eigenbase[:, :k].T
 
 
-def compress_images(flattened_images: np.array, eigenbase: np.array, k: int) -> np.array:
+def compress_images_1DPCA(flattened_images: np.array, eigenbase: np.array, k: int) -> np.array:
     reduced_images = reduce_dimensions(flattened_images, eigenbase, k)
-    return reconstruct_images(reduced_images, eigenbase, k)
+    return reconstruct_images_1DPCA(reduced_images, eigenbase, k)
 
 
 # 2DPCA
@@ -112,6 +112,15 @@ def get_feature_vectors(image: np.array, eigenbase: np.array, k: int) -> np.arra
     return image @ eigenbase[:, :k]
 
 
+def reconstruct_image_2DPCA(feature_vectors: np.array, eigenbase: np.array, k: int) -> np.array:
+    return feature_vectors @ eigenbase[:, :k].T
+
+
+def compress_single_image_2DPCA(image: np.array, eigenbase: np.array, k: int) -> np.array:
+    feature_vectors = get_feature_vectors(image, eigenbase, k)
+    return reconstruct_image_2DPCA(feature_vectors, eigenbase, k)
+
+
 parser = argparse.ArgumentParser("process_images")
 parser.add_argument("-use_smaller_images",
                     help="Decrease image resolution for faster computation time",
@@ -127,25 +136,27 @@ images = read_images(Path(faces_path), args.use_smaller_images, args.scale_down_
 h, w = images.shape[1], images.shape[2]
 EIGENBASE_SIZE = 100
 
-print("Calculating covariance...")
-flattened_images = flatten_images(images)
-covariance = get_1d_covariance_matrix(flattened_images)
-print("flattened_images shape: {}".format(flattened_images.shape))
+# print("Calculating covariance...")
+# flattened_images = flatten_images(images)
+# covariance = get_1d_covariance_matrix(flattened_images)
+# print("flattened_images shape: {}".format(flattened_images.shape))
 
-print("Calculating eigenvector base...")
-eigenvalues, eigenbase = get_eigenvalues_and_eigenvectors(covariance, EIGENBASE_SIZE, filename="amogus")
+# print("Calculating eigenvector base...")
+# eigenvalues, eigenbase = get_eigenvalues_and_eigenvectors(covariance, EIGENBASE_SIZE, filename="amogus")
 
-print("Compressing images...")
-compressed_images = compress_images(flattened_images, eigenbase, 30)
+# print("Compressing images...")
+# compressed_images = compress_images_1DPCA(flattened_images, eigenbase, 30)
 
-plt.imshow(compressed_images[0].reshape(h,w))
-plt.show()
-
-# print("Calculating image feature vectors...")
-# eigenvalues, eigenbase = get_eigenbase_for_images(images, 0, get_all=True, filename="amogus")
-# feature_vectors = get_feature_vectors(images[0], eigenbase, 46)
-# plt.imshow(feature_vectors)
+# plt.imshow(compressed_images[0].reshape(h,w))
 # plt.show()
+
+print("Calculating eigenbase for 2DPCA...")
+eigenvalues, eigenbase = get_eigenbase_for_images(images, 0, get_all=True, filename="amogus")
+
+print("Compressing image...")
+compressed_image = compress_single_image_2DPCA(images[0], eigenbase, 20)
+plt.imshow(compressed_image)
+plt.show()
 
 # f, axs = plt.subplots(3, 3, figsize=(12,12))
 # for i, ax in enumerate(axs.flatten()):
