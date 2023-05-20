2105bb329d5c2d464e4ad700ff7606dbc8527bc2
araml <lraffo@dc.uba.ar>
2023-05-19 21:03:43 -0300

    Similarity matrix

---
+++ TP2/py_stuff/similarity.py
@@ -0,0 +1,14 @@
+import numpy as np
+from support import read_images
+from pathlib import Path
+from image_paths import * # TODO: rename image_paths since it also contains matrix path now
+import matplotlib.pyplot as plt
+
+def similarity_matrix(data: np.array, similarity_function) -> np.array:
+    return np.corrcoef(data)
+
+images = read_images(Path(faces_path), True, 8)
+similarity = similarity_matrix(images[0:3].reshape(3, -1), lambda x,y: x)
+plt.pcolor(similarity, cmap='GnBu')
+plt.show()
+
