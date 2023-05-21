5d37d2331e66d75330d72db6eff4e84eb02d815e
Catalina Juarros <catalinajuarros@gmail.com>
2023-05-21 16:32:03 -0300

    Move figure generation functions to separate file

---
+++ TP2/py_stuff/figure_generation.py
@@ -0,0 +1,65 @@
+from image_paths import *
+from matplotlib import pyplot as plt
+import numpy as np
+from pathlib import Path
+from PCA import PCA
+from random import choices
+from string import ascii_letters
+
+def save_eigenvector_figure(eigenvectors: np.array,
+                            image_height: int,
+                            image_width: int,
+                            subplots_height: int,
+                            subplots_width: int,
+                            figsize: (int, int),
+                            filename=None,
+                            colourmap=plt.cm.viridis) -> str:
+    if not filename:
+        filename = ''.join(choices(ascii_letters, k=12))
+    file_path = Path(figures_path, filename + '.png')
+
+    _, axs = plt.subplots(subplots_height, subplots_width, figsize=figsize)
+    for i, ax in enumerate(axs.flatten()):
+        ax.imshow(eigenvectors[:,i].reshape(image_height, image_width), cmap=colourmap)
+        ax.set_title("Autovector {}".format(i+1))
+        ax.axis('off')
+    plt.tight_layout()
+    plt.savefig(file_path)
+    return file_path
+
+
+def save_image_comparison(original_image: np.array,
+                          compressed_image: np.array,
+                          image_height: int,
+                          image_width: int,
+                          figsize: (int, int),
+                          filename=None,
+                          colourmap=plt.cm.viridis) -> str:
+    if not filename:
+        filename = ''.join(choices(ascii_letters, k=12))
+    file_path = Path(figures_path, filename + '.png')
+    _, axs = plt.subplots(1, 2, figsize=figsize)
+
+    axs[0].imshow(original_image.reshape(image_height, image_width), cmap=colourmap)
+    axs[0].set_title("Imagen original")
+    axs[0].axis('off')
+
+    axs[1].imshow(compressed_image.reshape(image_height, image_width), cmap=colourmap)
+    axs[1].set_title("Imagen comprimida")
+    axs[1].axis('off')
+
+    plt.tight_layout()
+    plt.savefig(file_path)
+    return file_path
+
+
+def create_pca_image_comparison(pca: PCA,
+                                images: np.array,
+                                image_index: int,
+                                number_of_components: int,
+                                figsize: (int, int),
+                                colourmap=plt.cm.viridis) -> str:
+    h, w = images.shape[1], images.shape[2]
+    original_image, compressed_image = pca.get_image_comparison(images, image_index, number_of_components)
+    filename = "image_comparison_{}_{}components_{}".format(image_index, number_of_components, colourmap.name)
+    return save_image_comparison(original_image, compressed_image, h, w, figsize, filename, colourmap)
--- TP2/py_stuff/process_images.py
+++ TP2/py_stuff/process_images.py
@@ -1,10 +1,9 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+from figure_generation import *
 from image_paths import * # TODO: rename image_paths since it also contains matrix path now
 from pathlib import Path
-from random import choices
-from string import ascii_letters
 from utilities import * 
 from PCA import PCA
 
@@ -39,66 +38,6 @@ def compress_single_image_2DPCA(image: np.array, eigenbase: np.array, k: int) ->
     feature_vectors = get_feature_vectors(image, eigenbase, k)
     return reconstruct_image_2DPCA(feature_vectors, eigenbase, k)
 
-
-# Figures
-
-def save_eigenvector_figure(eigenvectors: np.array,
-                            image_height: int,
-                            image_width: int,
-                            subplots_height: int,
-                            subplots_width: int,
-                            figsize: (int, int),
-                            filename=None,
-                            colourmap=plt.cm.viridis) -> str:
-    if not filename:
-        filename = ''.join(choices(ascii_letters, k=12))
-    file_path = Path(figures_path, filename + '.png')
-
-    _, axs = plt.subplots(subplots_height, subplots_width, figsize=figsize)
-    for i, ax in enumerate(axs.flatten()):
-        ax.imshow(eigenvectors[:,i].reshape(image_height, image_width), cmap=colourmap)
-        ax.set_title("Autovector {}".format(i+1))
-        ax.axis('off')
-    plt.tight_layout()
-    plt.savefig(file_path)
-    return file_path
-
-
-def save_image_comparison(original_image: np.array,
-                          compressed_image: np.array,
-                          image_height: int,
-                          image_width: int,
-                          figsize: (int, int),
-                          filename=None,
-                          colourmap=plt.cm.viridis) -> str:
-    if not filename:
-        filename = ''.join(choices(ascii_letters, k=12))
-    file_path = Path(figures_path, filename + '.png')
-    _, axs = plt.subplots(1, 2, figsize=figsize)
-
-    axs[0].imshow(original_image.reshape(image_height, image_width), cmap=colourmap)
-    axs[0].set_title("Imagen original")
-    axs[0].axis('off')
-
-    axs[1].imshow(compressed_image.reshape(image_height, image_width), cmap=colourmap)
-    axs[1].set_title("Imagen comprimida")
-    axs[1].axis('off')
-
-    plt.tight_layout()
-    plt.savefig(file_path)
-    return file_path
-
-
-def create_pca_image_comparison(pca: PCA,
-                                images: np.array,
-                                image_index: int,
-                                number_of_components: int,
-                                figsize: (int, int),
-                                colourmap=plt.cm.viridis) -> str:
-    h, w = images.shape[1], images.shape[2]
-    original_image, compressed_image = pca.get_image_comparison(images, image_index, number_of_components)
-    filename = "image_comparison_{}_{}components_{}".format(image_index, number_of_components, colourmap.name)
-    return save_image_comparison(original_image, compressed_image, h, w, figsize, filename, colourmap)
 
 
 parser = argparse.ArgumentParser("process_images")
