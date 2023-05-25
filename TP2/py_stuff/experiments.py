e2486c1f72dc83839eccc9794ddf4cffae7297c8
araml <leandrojavr@gmail.com>
2023-05-25 00:40:45 -0300

    Similarity graph, maybe

--- TP2/py_stuff/experiments.py
+++ TP2/py_stuff/experiments.py
@@ -4,11 +4,21 @@ from pathlib import Path
 from data_paths import * # TODO: rename data_paths since it also contains matrix path now
 import matplotlib.pyplot as plt
 
-def similarity_matrix(data: np.array, similarity_function) -> np.array:
-    return np.corrcoef(data)
+# Maybe I'm not understanding this one, but should we reconstruct everyones 
+# images and then compare them?
+def similarity_analysis(one_person: np.array, rest: np.array, Ks: list) -> None
+    for k in Ks:
+        # Reconstruct with k 
+        # single person/the rest 
+        similarity_matrix = np.corrcoef(one_person, rest)
+        plt.pcolor(similarity, cmap='GnBu')
+        plt.title(f'Similarity matrix with k = {k}')
+        plt.savefig(f'Similarity matrix with k = {k}')
 
+
+# example on how to use corrcoef
 images = read_images(Path(faces_path), True, 8)
-similarity = similarity_matrix(images[0:3].reshape(3, -1), lambda x,y: x)
+similarity = np.corrcoef(images[0:3].reshape(3, -1))
 plt.pcolor(similarity, cmap='GnBu')
 plt.show()
 
