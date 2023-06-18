e95d64f40b8c8cad99e9ec2b6ccad6b39f224885
araml <leandrojavr@gmail.com>
2023-06-17 22:41:24 -0300

    Folder structure

---
+++ TP3/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+mkdir -p build
+cd build
+
+# get number of available cores 
+cores=4
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cores=$(sysctl -n hw.physicalcpu)
+else
+    cores=$(nproc --all)
+fi
+
+if ! command -v ninja &> /dev/null
+then
+    cmake -DUNITY=ON -DBUILD_TESTS=ON .. 
+    make -j $cores
+else
+    cmake -DUNITY=ON -DBUILD_TESTS=ON -G Ninja .. 
+    ninja
+fi
+
+echo "Copying deflate to Python folder"
+cp metodos_iterativos.cpython* ../py/
+echo "Copied deflate to Python folder"
---
+++ TP3/init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://gitlab.com/libeigen/eigen
+git clone https://github.com/pybind/pybind11
