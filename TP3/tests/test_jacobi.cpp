293482359c31f58b8796b5d96e3931458f322261
Catalina Juarros <catalinajuarros@gmail.com>
2023-06-20 13:00:49 -0300

    Write tests for sum version of Jacobi

--- TP3/tests/test_jacobi.cpp
+++ TP3/tests/test_jacobi.cpp
@@ -28,6 +28,24 @@ void test_diagonal_zeros_matrix() {
     assert(0);
 }
 
+void test_diagonal_zeros_sum() {
+    MatrixXd M(2, 2);
+    M << 0, 1, 1, 1;
+    VectorXd b = ones(2);
+
+    try {
+        auto j = jacobi_sum(M, b);
+    } catch (std::logic_error const& ex) {
+        const char* message = ex.what();
+        const char* expected = "Matrix has zeros in diagonal";
+        assert(strcmp(message, expected) == 0);
+        std::cout << "Matrix with zeros in diagonal (sum) OK" << std::endl;
+        return;
+    }
+
+    assert(0);
+}
+
 void test_non_convergent_matrix() {
     MatrixXd M(2, 2);
     M << 0.5, 1, 1, 0.5;
@@ -40,6 +58,24 @@ void test_non_convergent_matrix() {
         const char* expected = "Matrix does not converge";
         assert(strcmp(message, expected) == 0);
         std::cout << "Non-convergent matrix OK" << std::endl;
+        return;
+    }
+
+    assert(0);
+}
+
+void test_non_convergent_sum() {
+    MatrixXd M(2, 2);
+    M << 0.5, 1, 1, 0.5;
+    VectorXd b = ones(2);
+    
+    try {
+        auto j = jacobi_sum(M, b);
+    } catch (std::logic_error const& ex) {
+        const char* message = ex.what();
+        const char* expected = "Matrix does not converge";
+        assert(strcmp(message, expected) == 0);
+        std::cout << "Non-convergent matrix (sum) OK" << std::endl;
         return;
     }
 
@@ -59,8 +95,24 @@ void test_convergent_matrix() {
     std::cout << "Convergent matrix OK" << std::endl;
 }
 
+void test_convergent_sum() {
+    MatrixXd M(2, 2);
+    M << 2, 1, 1, 2;
+    VectorXd b = ones(2);
+    
+    Eigen::FullPivLU<Eigen::MatrixXd> lu(M);
+    auto expected = gaussianElimination(M, b);
+    auto j = jacobi_sum(M, b, 1000, EPSILON);
+
+    assert((expected-j).norm() < EPSILON);
+    std::cout << "Convergent matrix OK" << std::endl;
+}
+
 int main() {
     test_diagonal_zeros_matrix();
+    test_diagonal_zeros_sum();
     test_non_convergent_matrix();
+    test_non_convergent_sum();
     test_convergent_matrix();
+    test_convergent_sum();
 }
