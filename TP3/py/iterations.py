17dba23b2765e039f21a3e9b36e1cfb591435193
Catalina Juarros <catalinajuarros@gmail.com>
2023-06-24 19:38:57 -0300

    Add box plot figures and refactor CSV reading code

--- TP3/py/iterations.py
+++ TP3/py/iterations.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 from iterative_methods import *
-from utils import adjacent_values, create_test_case
+from utils import *
 
 
 def measure_iterations(iterative_method,
@@ -49,17 +49,11 @@ def violin_plot_iterations(factors_to_plot: list,
                            figure_filename: str,
                            scale: str,
                            bw = None) -> None:
-    data = []
-    x_values = []
-    with open(csv_filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        for row in reader:
-            factor, iterations = int(row[0]), row[1:]
-            if factor in factors_to_plot:
-                x_values.append(int(factor))
-                data.append([int(it) for it in iterations])
+    x_values, data = read_data_from_csv(factors_to_plot, csv_filename, int, int)
 
-    _, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,6))
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,6))
+    fig.tight_layout(pad=3)
+
     quartile1, medians, quartile3 = np.percentile(data, [25, 50, 75], axis=1)
     whiskers = np.array([adjacent_values(sorted_array, q1, q3)
                          for sorted_array, q1, q3 in zip(data, quartile1, quartile3)])
@@ -74,7 +68,23 @@ def violin_plot_iterations(factors_to_plot: list,
     ax.set_xticks(x_values)
     ax.violinplot(data, x_values, showmeans=False, showmedians=False, bw_method=bw)
     plt.savefig(figure_filename)
-    plt.clf()
+    plt.close()
+
+
+def box_plot_iterations(factors_to_plot: list,
+                        csv_filename: str,
+                        figure_filename: str,
+                        scale: str = "linear"):
+    x_values, data = read_data_from_csv(factors_to_plot, csv_filename, int, int)
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 6))
+    fig.tight_layout(pad=3)
+    ax.set_yscale(scale)
+    ax.set_xlabel("Factor de expansión de la diagonal")
+    ax.set_ylabel("Cantidad de iteraciones hasta converger")
+    ax.boxplot(data, showmeans=True, meanline=True, positions=x_values,
+               flierprops={"marker": '.'})
+    plt.savefig(figure_filename)
+    plt.close()
 
 
 def line_plot_iterations(methods: list,
@@ -85,17 +95,12 @@ def line_plot_iterations(methods: list,
     ax1 = plt.subplot()
     for method in methods:
         data = {}
-        with open(f"{method}_iterations.csv", newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=';')
-            for row in reader:
-                factor, iterations = int(row[0]), row[1:]
-                if factor in factors_to_plot:
-                    data[factor] = np.mean([int(m) for m in iterations])
-            factors = list(data.keys())
-            plt.plot(factors, [data[f] for f in factors], '-o', label=method)
-            ax1.set_xticks(factors)
-            if rotate_xticklabels:
-                ax1.set_xticklabels([str(f) for f in factors], rotation=90)
+        x_values, data = read_data_from_csv(
+        factors_to_plot, f"{method}_iterations.csv", int, int)
+        plt.plot(x_values, [np.mean(d) for d in data], '-o', label=method)
+        ax1.set_xticks(x_values)
+        if rotate_xticklabels:
+            ax1.set_xticklabels([str(f) for f in x_values], rotation=90)
 
     plt.legend()
     plt.yscale(scale)
@@ -103,7 +108,7 @@ def line_plot_iterations(methods: list,
     plt.ylabel("Cantidad promedio de iteraciones hasta converger")
     plt.tight_layout()
     plt.savefig(figure_filename)
-    plt.clf()
+    plt.close()
 
 
 REPETITIONS = 100
@@ -115,7 +120,7 @@ GS_MATRIX_RANGE = range(10, 1001)
 JACOBI_SUM_RANGE = range(120, 241)
 JACOBI_MATRIX_RANGE = range(120, 241)
 
-GS_VIOLIN_RANGES = [range(15, 21), range(20, 26), range(50, 56), range(100, 106)]
+GS_BOX_RANGES = [range(15, 21), range(20, 26), range(50, 56), range(100, 106)]
 
 # === GAUSS-SEIDEL SUM === #
 measure_iterations_growing_diagonal(
@@ -123,10 +128,19 @@ measure_iterations_growing_diagonal(
     GS_SUM_RANGE, "gs_sum_iterations.csv")
 
 violin_plot_iterations(
-    range(10, 16), f"gs_sum_iterations.csv", f"gs_sum_iterations_10_15.png", "log")
-for rg in GS_VIOLIN_RANGES:
-    violin_plot_iterations(
-        rg, f"gs_sum_iterations.csv", f"gs_sum_iterations_{rg[0]}_{rg[-1]}.png", "linear")
+    range(10, 16), "gs_sum_iterations.csv",
+    "violin_gs_sum_iterations_10_15.png", "log")
+
+box_plot_iterations(range(10, 16), "gs_sum_iterations.csv",
+                    "box_gs_sum_iterations_10_15.png", "log")
+
+for rg in GS_BOX_RANGES:
+    violin_plot_iterations(rg, "gs_sum_iterations.csv",
+                           f"violin_gs_sum_iterations_{rg[0]}_{rg[-1]}.png",
+                           "linear")
+    box_plot_iterations(rg, "gs_sum_iterations.csv",
+                        f"box_gs_sum_iterations_{rg[0]}_{rg[-1]}.png", "linear")
+
 
 # === GAUSS-SEIDEL MATRIX === #
 measure_iterations_growing_diagonal(
@@ -134,38 +148,64 @@ measure_iterations_growing_diagonal(
     GS_MATRIX_RANGE, "gs_matrix_iterations.csv")
 
 violin_plot_iterations(
-    range(10, 16), f"gs_matrix_iterations.csv", f"gs_matrix_iterations_10_15.png", "log")
-for rg in GS_VIOLIN_RANGES:
-    violin_plot_iterations(
-        rg, f"gs_matrix_iterations.csv", f"gs_matrix_iterations_{rg[0]}_{rg[-1]}.png", "linear")
-    
-line_plot_iterations(["gs_matrix", "gs_sum"], range(10, 26), "gs_iterations.png")
+    range(10, 16), f"gs_matrix_iterations.csv",
+    "gs_matrix_iterations_10_15.png", "log")
 
-# === JACOBI SUM === #
+box_plot_iterations(range(10, 16), "gs_matrix_iterations.csv",
+                    "box_gs_matrix_iterations_10_15.png", "log")
+
+for rg in GS_BOX_RANGES:
+    violin_plot_iterations(rg, "gs_matrix_iterations.csv",
+                           f"violin_gs_matrix_iterations_{rg[0]}_{rg[-1]}.png",
+                           "linear")
+    box_plot_iterations(rg, "gs_matrix_iterations.csv",
+                        f"box_gs_matrix_iterations_{rg[0]}_{rg[-1]}.png", "linear")
+    
+line_plot_iterations(["gs_matrix", "gs_sum"], range(10, 26),
+                     "line_gs_iterations.png")
+
+# # === JACOBI SUM === #
 measure_iterations_growing_diagonal(
     jacobi_sum_method, DIMENSION, REPETITIONS, LOW, HIGH,
     JACOBI_SUM_RANGE, "jacobi_sum_iterations.csv")
 
 violin_plot_iterations(
     range(120, 126), f"jacobi_sum_iterations.csv",
-    f"jacobi_sum_iterations_120_125.png", "linear")
+    "violin_jacobi_sum_iterations_120_125.png", "linear")
 
 violin_plot_iterations(
     range(120, 145, 5), f"jacobi_sum_iterations.csv",
-    f"jacobi_sum_iterations_120_140.png", "linear", 10000)
+    "violin_jacobi_sum_iterations_120_140.png", "linear", 10000)
 
-# == JACOBI MATRIX == #
+box_plot_iterations(
+    range(120, 126), f"jacobi_sum_iterations.csv",
+    "box_jacobi_sum_iterations_120_125.png", "linear")
+
+box_plot_iterations(
+    range(120, 145, 5), f"jacobi_sum_iterations.csv",
+    "box_jacobi_sum_iterations_120_140.png", "linear")
+
+
+# # == JACOBI MATRIX == #
 measure_iterations_growing_diagonal(
     jacobi_matrix, DIMENSION, REPETITIONS, LOW, HIGH,
     JACOBI_MATRIX_RANGE, "jacobi_matrix_iterations.csv")
 
 violin_plot_iterations(
-    range(120, 126), f"jacobi_matrix_iterations.csv",
-    f"jacobi_matrix_iterations_120_125.png", "linear")
+    range(120, 126), "jacobi_matrix_iterations.csv",
+    "violin_jacobi_matrix_iterations_120_125.png", "linear")
 
 violin_plot_iterations(
-    range(120, 145, 5), f"jacobi_matrix_iterations.csv",
-    f"jacobi_matrix_iterations_120_140.png", "linear", 10000)
+    range(120, 145, 5), "jacobi_matrix_iterations.csv",
+    "violin_jacobi_matrix_iterations_120_140.png", "linear", 10000)
 
-line_plot_iterations(range(120, 146), "jacobi_sum_iterations.csv",
-                     "jacobi_matrix_iterations.png", rotate_xticklabels=True)
+box_plot_iterations(
+    range(120, 126), f"jacobi_matrix_iterations.csv",
+    "box_jacobi_matrix_iterations_120_125.png", "linear")
+
+box_plot_iterations(
+    range(120, 145, 5), f"jacobi_matrix_iterations.csv",
+    "box_jacobi_matrix_iterations_120_140.png", "linear")
+
+line_plot_iterations(["jacobi_matrix", "jacobi_sum"], range(120, 146),
+                     "line_jacobi_iterations.png", rotate_xticklabels=True)
--- TP3/py/utils.py
+++ TP3/py/utils.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 
 def create_test_case(dimension: int, low: int, high: int, diagonal_expansion_factor=None):
@@ -18,3 +19,15 @@ def adjacent_values(vals, q1, q3):
     lower_adjacent_value = q1 - (q3 - q1) * 1.5
     lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
     return lower_adjacent_value, upper_adjacent_value
+
+def read_data_from_csv(keys_to_read: list, csv_filename: str, key_type, value_type):
+    x_values = []
+    data = []
+    with open(csv_filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            key, values = int(row[0]), row[1:]
+            if key in keys_to_read:
+                x_values.append(key_type(key))
+                data.append([value_type(v) for v in values])
+    return x_values, data
