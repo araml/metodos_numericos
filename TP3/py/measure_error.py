49cfac64bc40c4d1b73031cd3f5363fdd4938314
araml <leandrojavr@gmail.com>
2023-06-24 14:31:03 -0300

    Error experiment

--- TP3/py/measure_error.py
+++ TP3/py/measure_error.py
@@ -1,7 +1,38 @@
-import iterative_methods
 import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+from iterative_methods import *
 
 def measure_n2_error(iterative_method_to_measure, m, x_0, b, iterations, eps) -> float:
     expected_result = iterative_methods.gaussian_elimination(m, b)
     actual_result, _ = iterative_method_to_measure(m, x_0, b, iterations, eps)
     return np.linalg.norm(expected_result-actual_result, ord=2)
+
+def run_error_experiment(max_dim: int = 1000):
+    steps = int(max_dim/10)
+    jm = js = gsm = gss = []
+    for dim in range(steps, max_dim, steps):
+        m, x, b = try_create_convergent_matrix(dim)
+        v1, _ = jacobi_matrix(m, b, x)
+        jm.append(abs(v1 - b))
+        v2, _ = jacobi_sum_method(m, b, x)
+        js.append(abs(v2 - b))
+        v3, _ = gauss_seidel_matrix(m, b, x)
+        gsm.append(abs(v3 - b))
+        v4, _ = gauss_seidel_sum_method(m, b, x)
+        gss.append(abs(v4 - b))
+     
+
+    xs = range(0, max_dim, steps)
+    axes.plot(xs, jm, '-o',                           
+              label=f'Jacobi matriz')       
+    axes.plot(xs, js, '-o',                                
+              label=f'Jacobi suma')            
+    axes.plot(xs, gsm, '-o',                      
+              label=f'Gauss-seidel matriz')      
+    axes.plot(xs, gss, '-o',                           
+              label=f'Gauss-seidel suma')       
+    
+
+if __name__ == '__main__':
+    run_error_experiment()
--- TP3/py/utils.py
+++ TP3/py/utils.py
@@ -9,3 +9,35 @@ def create_test_case(dimension: int, low: int, high: int, diagonal_expansion_fac
             m[i, i] *= diagonal_expansion_factor
     x = np.random.randint(low=low, high=high, size=dimension)
     return (m, x, m@x)
+
+# The idea is to create a diagonal matrix D whose entries are all < 1 and then 
+# try to create another matrix P that is inversible, because we know that 
+# similar matrix have the same eigenvalues we can create a new matrix (That is
+# not only diagonal nor DDM) P * M * P^-1
+# Note: the resulting matrix might not be invertible, although it most likely is
+# going to be, in fact we could force it to be singular by adding a zeroe'd eigenvalue.
+def try_create_convergent_matrix(dimension: int, low: float = 0, high: float =
+                                 1000, seed: float = None):
+
+    if seed:
+        np.random.seed(seed)
+
+    matrix_dim = (dimension, dimension)
+    D = np.zeros(matrix_dim)
+    for i in range(dimension):
+        D[i, i] = np.random.random()
+
+    P = P_inv = D
+
+    # Probability of a singular matrix is 0 so we should find an invertible one
+    # fairly fast.
+    while True:
+        P = np.random.randint(low = low, high = high, size = matrix_dim)
+        try: 
+            P_inv = np.linalg.inv(P)
+            break
+        except: 
+            continue
+    M = P@D@P_inv
+    x = np.random.randint(low, high, size = dimension)
+    return (M, x, M@x)
