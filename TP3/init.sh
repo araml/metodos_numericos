1830a7f9d5dea5097d47174c244814cf48be8baa
Catalina Juarros <catalinajuarros@gmail.com>
2023-06-25 11:34:38 -0300

    Create data paths file and folders

--- TP3/init.sh
+++ TP3/init.sh
@@ -3,3 +3,17 @@
 git clone https://gitlab.com/libeigen/eigen
 git clone https://github.com/pybind/pybind11
 git clone https://github.com/doctest/doctest/
+
+if [ -d "./csvs" ]
+then
+    echo "CSV folder already exists"
+else
+    mkdir csvs
+fi
+
+if [ -d "./figures" ]
+then
+    echo "Figure folder already exists"
+else
+    mkdir figures
+fi
---
+++ TP3/py/data_paths.in.py
@@ -0,0 +1,2 @@
+csvs_path = '@CSVS_PATH@'
+figures_path = '@FIGURES_PATH@'
