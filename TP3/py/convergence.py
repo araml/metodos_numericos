605070a387b1ae6b12b36a1ef9822c0d1bca3792
Catalina Juarros <catalinajuarros@gmail.com>
2023-06-26 19:30:04 -0300

    Clean up plotting code

--- TP3/py/convergence.py
+++ TP3/py/convergence.py
@@ -74,15 +74,13 @@ def bar_plot_convergence(methods,
         method_data.append(df)
     means = []
     for df in method_data:
-        print(iterations)
-        print(df)
         method = df["method"].iloc[0]
         mean = df.groupby("factor").mean(numeric_only=True)
         means.append(mean.assign(method=method))
     df = pd.concat(means)
     g = sns.barplot(data=df, x=df.index, y="converged", hue="method")
     plt.xlabel("Factor de expansión de la diagonal")
-    plt.ylabel("Porcentaje de casos en los que el método converge")
+    plt.ylabel("Proporción de casos en los que el método converge")
     plt.tight_layout()
     plt.yscale(scale)
     plt.savefig(os.path.join(figures_path, filename))
@@ -95,37 +93,34 @@ LOW = 1
 HIGH = 5
 FACTORS = range(10, 1001)
 ITERATIONS = range(10, 51, 10)
-GS_PLOT_PARAMS = [
-    (10, range(90, 220, 20)),
-    (20, range(40, 75, 5)),
-    (30, range(24, 50, 4)),
-    (100, range(8))]
+GS_BAR_PLOT_PARAMS = [
+    (10, range(100, 210, 10)),
+    (100, range(10, 21))]
+JACOBI_BAR_PLOT_PARAMS = [
+    (10, range(250, 800, 50)),
+    (100, range(125, 171, 4))
+]
 
-# measure_convergence_ratio_growing_diagonal(
-#         "gauss_seidel_sum_method", DIMENSION, REPETITIONS,
-#         LOW, HIGH, FACTORS, None, 100)
-# measure_convergence_ratio_growing_diagonal(
-#         "gauss_seidel_matrix", DIMENSION, REPETITIONS,
-#         LOW, HIGH, FACTORS, None, 100)
-
-for its, factors_to_plot in GS_PLOT_PARAMS:
-    # measure_convergence_ratio_growing_diagonal(
-    #     "gauss_seidel_sum_method", DIMENSION, REPETITIONS,
-    #     LOW, HIGH, factors_to_plot, None, its)
-    # measure_convergence_ratio_growing_diagonal(
-    #     "jacobi_sum_method", DIMENSION, REPETITIONS,
-    #     LOW, HIGH, factors_to_plot, None, its)
-    # measure_convergence_ratio_growing_diagonal(
-    #     "gauss_seidel_matrix", DIMENSION, REPETITIONS,
-    #     LOW, HIGH, factors_to_plot, None, its)
-    # measure_convergence_ratio_growing_diagonal(
-    #     "jacobi_matrix", DIMENSION, REPETITIONS,
-    #     LOW, HIGH, factors_to_plot, None, its)
+for its, factors_to_plot in GS_BAR_PLOT_PARAMS:
+    measure_convergence_ratio_growing_diagonal(
+            "gauss_seidel_sum_method", DIMENSION, REPETITIONS,
+            LOW, HIGH, FACTORS, None, its)
+    measure_convergence_ratio_growing_diagonal(
+            "gauss_seidel_matrix", DIMENSION, REPETITIONS,
+            LOW, HIGH, FACTORS, None, its)
     bar_plot_convergence(
         ["gauss_seidel_sum_method", "gauss_seidel_matrix"],
         factors_to_plot,
         f"gauss_seidel_convergence_dim{DIMENSION}_{its}its.png", its)
-    # bar_plot_convergence(
-    #     ["jacobi_sum_method", "jacobi_matrix"],
-    #     factors,
-    #     f"jacobi_convergence_dim{DIMENSION}_{its}its.png", its)
+
+for its, factors_to_plot in JACOBI_BAR_PLOT_PARAMS:
+    measure_convergence_ratio_growing_diagonal(
+            "jacobi_sum_method", DIMENSION, REPETITIONS,
+            LOW, HIGH, FACTORS, None, its)
+    measure_convergence_ratio_growing_diagonal(
+            "jacobi_matrix", DIMENSION, REPETITIONS,
+            LOW, HIGH, FACTORS, None, its)
+    bar_plot_convergence(
+        ["jacobi_sum_method", "jacobi_matrix"],
+        factors_to_plot,
+        f"jacobi_convergence_dim{DIMENSION}_{its}its.png", its)
