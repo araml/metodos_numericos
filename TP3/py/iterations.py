74994d77379512abe05af96e52621def3637dd96
Catalina Juarros <catalinajuarros@gmail.com>
2023-06-25 22:08:18 -0300

    Improve figures

--- TP3/py/iterations.py
+++ TP3/py/iterations.py
@@ -50,14 +50,14 @@ def measure_iterations_growing_diagonal(iterative_method,
     df.to_csv(full_path, sep='\t', index=False)
 
 
-def violin_plot_iterations(factors_to_plot: list,
-                           csv_filename: str,
+def violin_plot_iterations(methods: list,
+                           factors_to_plot: list,
                            figure_filename: str,
                            y_scale: str) -> None:
-    df = pd.read_csv(os.path.join(csvs_path, csv_filename), sep='\t')
-    df = df[df["factor"].isin(factors_to_plot)]
+    method_data = read_method_datasets_from_csv(methods, factors_to_plot)
     plt.figure(figsize=(8,6))
-    g = sns.violinplot(df, x="factor", y="iterations", color="tab:orange")
+    g = sns.violinplot(data=pd.concat(method_data), x="factor",
+                       y="iterations", hue="method")
     g.set_yscale(y_scale)
     g.set_xlabel("Factor de expansión de la diagonal")
     g.set_ylabel("Cantidad de iteraciones hasta converger")
@@ -65,14 +65,14 @@ def violin_plot_iterations(factors_to_plot: list,
     plt.close()
 
 
-def box_plot_iterations(factors_to_plot: list,
-                        csv_filename: str,
+def box_plot_iterations(methods: list,
+                        factors_to_plot: list,
                         figure_filename: str,
                         y_scale: str = "linear"):
-    df = pd.read_csv(os.path.join(csvs_path, csv_filename), sep='\t')
-    df = df[df["factor"].isin(factors_to_plot)]
+    method_data = read_method_datasets_from_csv(methods, factors_to_plot)
     plt.figure(figsize=(8,6))
-    g = sns.boxplot(df, x="factor", y="iterations", color="tab:orange",
+    g = sns.boxplot(data=pd.concat(method_data), x="factor",
+                    y="iterations", hue="method",
                     flierprops={"marker": '.'})
     g.set_yscale(y_scale)
     g.set_xlabel("Factor de expansión de la diagonal")
@@ -89,21 +89,17 @@ def line_plot_iterations(methods: list,
                          remove_outliers: bool = False,
                          low_q: float = 0.25,
                          high_q: float = 0.75) -> None:
-    datasets = []
-    for method in methods:
-        df = pd.read_csv(os.path.join(csvs_path, f"{method}_iterations.csv"),
-                         sep='\t')
-        df = df[df["factor"].isin(factors_to_plot)]
-        if remove_outliers:
-            df = df[df.groupby("factor").iterations.\
-                transform(lambda x : (x < x.quantile(high_q)) & \
-                          (x > x.quantile(low_q))).eq(1)]
-        iteration_means = df.groupby("factor").mean()
-        datasets.append(iteration_means.assign(method=method))
+    method_data = read_method_datasets_from_csv(
+        methods, factors_to_plot, remove_outliers, low_q, high_q)
+    means = []
+    for df in method_data:
+        method = df["method"].iloc[0]
+        mean = df.groupby("factor").mean(numeric_only=True)
+        means.append(mean.assign(method=method))
 
-    g = sns.lineplot(pd.concat(datasets), x="factor", y="iterations",
+    g = sns.lineplot(pd.concat(means), x="factor", y="iterations",
                      hue="method", marker='o')
-    g.set_xticks(datasets[0].axes[0].to_list())
+    g.set_xticks(means[0].axes[0].to_list())
     if rotate_xticklabels:
         g.tick_params(axis='x', rotation=90)
     plt.yscale(scale)
@@ -114,6 +110,26 @@ def line_plot_iterations(methods: list,
     plt.close()
 
 
+def read_method_datasets_from_csv(methods: list,
+                                  factors_to_plot: list,
+                                  remove_outliers: bool = False,
+                                  low_q: float = 0.25,
+                                  high_q: float = 0.75) -> pd.DataFrame:
+    method_data = []
+    for method in methods:
+        df = pd.read_csv(os.path.join(csvs_path, f"{method}_iterations.csv"),
+                         sep='\t')
+        df = df[df["factor"].isin(factors_to_plot)]
+        # TODO: use a better method for detecting outliers
+        if remove_outliers:
+            df = df[df.groupby("factor").iterations.\
+                transform(lambda x : (x < x.quantile(high_q)) & \
+                          (x > x.quantile(low_q))).eq(1)]
+        df["method"] = method
+        method_data.append(df)
+    return method_data
+
+
 REPETITIONS = 100
 DIMENSION = 100
 LOW = 1
@@ -122,95 +138,46 @@ GS_SUM_RANGE = range(10, 1001)
 GS_MATRIX_RANGE = range(10, 1001)
 JACOBI_SUM_RANGE = range(120, 241)
 JACOBI_MATRIX_RANGE = range(120, 241)
+JACOBI_VS_GS_RANGE = range(120, 241, 5)
+JACOBI_BOXPLOT_RANGE = range(120, 241, 15)
 
-GS_BOX_RANGES = [range(15, 21), range(20, 26), range(50, 56), range(100, 106)]
-
-# === GAUSS-SEIDEL SUM === #
+# === GAUSS-SEIDEL === #
 measure_iterations_growing_diagonal(
     gauss_seidel_sum_method, DIMENSION, REPETITIONS, LOW, HIGH,
     GS_SUM_RANGE, "gs_sum_iterations.csv")
 
-violin_plot_iterations(
-    range(10, 16), "gs_sum_iterations.csv",
-    "violin_gs_sum_iterations_10_15.png", "log")
-
-box_plot_iterations(range(10, 16), "gs_sum_iterations.csv",
-                    "box_gs_sum_iterations_10_15.png", "log")
-
-for rg in GS_BOX_RANGES:
-    violin_plot_iterations(rg, "gs_sum_iterations.csv",
-                           f"violin_gs_sum_iterations_{rg[0]}_{rg[-1]}.png",
-                           "linear")
-    box_plot_iterations(rg, "gs_sum_iterations.csv",
-                        f"box_gs_sum_iterations_{rg[0]}_{rg[-1]}.png", "linear")
-
-
-# === GAUSS-SEIDEL MATRIX === #
 measure_iterations_growing_diagonal(
     gauss_seidel_matrix, DIMENSION, REPETITIONS, LOW, HIGH,
     GS_MATRIX_RANGE, "gs_matrix_iterations.csv")
 
 violin_plot_iterations(
-    range(10, 16), f"gs_matrix_iterations.csv",
-    "gs_matrix_iterations_10_15.png", "log")
+    ["gs_sum", "gs_matrix"], range(10, 30, 2),
+    "violin_gs_iterations.png", "log")
 
-box_plot_iterations(range(10, 16), "gs_matrix_iterations.csv",
-                    "box_gs_matrix_iterations_10_15.png", "log")
+box_plot_iterations(["gs_sum", "gs_matrix"], range(10, 30, 2),
+                    "box_gs_iterations.png", "log")
 
-for rg in GS_BOX_RANGES:
-    violin_plot_iterations(rg, "gs_matrix_iterations.csv",
-                           f"violin_gs_matrix_iterations_{rg[0]}_{rg[-1]}.png",
-                           "linear")
-    box_plot_iterations(rg, "gs_matrix_iterations.csv",
-                        f"box_gs_matrix_iterations_{rg[0]}_{rg[-1]}.png", "linear")
-
-line_plot_iterations(["gs_matrix", "gs_sum"], range(10, 26),
+line_plot_iterations(["gs_matrix", "gs_sum"], range(10, 31),
                      "line_gs_iterations.png")
-    
-line_plot_iterations(["gs_matrix", "gs_sum"], range(10, 26),
+
+line_plot_iterations(["gs_matrix", "gs_sum"], range(10, 31),
                      "line_gs_iterations_no_outliers.png", remove_outliers=True)
 
-# === JACOBI SUM === #
+# === JACOBI === #
 measure_iterations_growing_diagonal(
     jacobi_sum_method, DIMENSION, REPETITIONS, LOW, HIGH,
     JACOBI_SUM_RANGE, "jacobi_sum_iterations.csv")
 
-violin_plot_iterations(
-    range(120, 126), "jacobi_sum_iterations.csv",
-    "violin_jacobi_sum_iterations_120_125.png", "linear")
-
-violin_plot_iterations(
-    range(120, 145, 5), "jacobi_sum_iterations.csv",
-    "violin_jacobi_sum_iterations_120_140.png", "linear")
-
-box_plot_iterations(
-    range(120, 126), "jacobi_sum_iterations.csv",
-    "box_jacobi_sum_iterations_120_125.png", "linear")
-
-box_plot_iterations(
-    range(120, 145, 5), "jacobi_sum_iterations.csv",
-    "box_jacobi_sum_iterations_120_140.png", "linear")
-
-# == JACOBI MATRIX == #
 measure_iterations_growing_diagonal(
     jacobi_matrix, DIMENSION, REPETITIONS, LOW, HIGH,
     JACOBI_MATRIX_RANGE, "jacobi_matrix_iterations.csv")
 
 violin_plot_iterations(
-    range(120, 126), "jacobi_matrix_iterations.csv",
-    "violin_jacobi_matrix_iterations_120_125.png", "linear")
+    ["jacobi_sum", "jacobi_matrix"], JACOBI_BOXPLOT_RANGE,
+    "violin_jacobi_iterations.png", "log")
 
-violin_plot_iterations(
-    range(120, 145, 5), "jacobi_matrix_iterations.csv",
-    "violin_jacobi_matrix_iterations_120_140.png", "linear")
-
-box_plot_iterations(
-    range(120, 126), "jacobi_matrix_iterations.csv",
-    "box_jacobi_matrix_iterations_120_125.png", "linear")
-
-box_plot_iterations(
-    range(120, 145, 5), "jacobi_matrix_iterations.csv",
-    "box_jacobi_matrix_iterations_120_140.png", "linear")
+box_plot_iterations(["jacobi_sum", "jacobi_matrix"], JACOBI_BOXPLOT_RANGE,
+                    "box_jacobi_iterations.png", "log")
 
 line_plot_iterations(["jacobi_matrix", "jacobi_sum"], range(120, 146),
                      "line_jacobi_iterations.png", rotate_xticklabels=True)
@@ -218,3 +185,31 @@ line_plot_iterations(["jacobi_matrix", "jacobi_sum"], range(120, 146),
 line_plot_iterations(["jacobi_matrix", "jacobi_sum"], range(120, 146),
                      "line_jacobi_iterations_no_outliers.png",
                      rotate_xticklabels=True, remove_outliers=True)
+
+# === JACOBI/GS COMPARISON === #
+
+violin_plot_iterations(
+    ["jacobi_sum", "gs_sum"], JACOBI_BOXPLOT_RANGE,
+    "violin_jacobi_vs_gs_sum_iterations.png", "log")
+
+box_plot_iterations(
+    ["jacobi_sum", "gs_sum"], JACOBI_BOXPLOT_RANGE,
+    "box_jacobi_vs_gs_sum_iterations.png", "log")
+
+line_plot_iterations(["jacobi_sum", "gs_sum"], JACOBI_VS_GS_RANGE,
+                     "line_jacobi_vs_gs_iterations_sum.png",
+                     rotate_xticklabels=True, remove_outliers=False,
+                     scale="log")
+
+violin_plot_iterations(
+    ["jacobi_matrix", "gs_matrix"], JACOBI_BOXPLOT_RANGE,
+    "violin_jacobi_vs_gs_matrix_iterations.png", "log")
+
+box_plot_iterations(
+    ["jacobi_matrix", "gs_matrix"], JACOBI_BOXPLOT_RANGE,
+    "box_jacobi_vs_gs_matrix_iterations.png", "log")
+
+line_plot_iterations(["jacobi_matrix", "gs_matrix"], JACOBI_VS_GS_RANGE,
+                     "line_jacobi_vs_gs_iterations_matrix.png",
+                     rotate_xticklabels=True, remove_outliers=False,
+                     scale="log")
